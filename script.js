0b9c548ed2c569cae334ebe386031d9e0c86d75e
adityag7522 <66068085+adityag7522@users.noreply.github.com>
2023-07-24 22:51:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	arr.sort(function(a,b){return (a-b)})
+	let j=0;
+	let k=0;
+	for(let i=0;i<arr.length;i++){
+		let j=i+1;
+		let k=arr.length-1;
+		let sum=0;
+		let ans=0;
+		while(i<j){
+			sum=arr[i]+arr[j]+arr[k];
+			let diff=sum-target;
+			if(sum<target){diff=target-sum;}
+			ans=Math.max(ans,diff);
+			if(sum==target){
+				return 0;
+			}
+			else if(sum<target){j++;}
+			else{k--;}
+		}
+	}
+  return ans;
 }
 
 module.exports = threeSum;
