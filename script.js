789494ea1a915a0360c438506a4ded96b0a21670
adityag7522 <66068085+adityag7522@users.noreply.github.com>
2023-07-24 23:02:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 function threeSum(arr, target) {
 // write your code here
-	arr.sort(function(a,b){return (a-b)})
-	let j=0;
-	let k=0;
-	for(let i=0;i<arr.length;i++){
-		let j=i+1;
-		let k=arr.length-1;
-		let sum=0;
-		let ans=0;
-		while(i<j){
-			sum=arr[i]+arr[j]+arr[k];
-			let diff=sum-target;
-			if(sum<target){diff=target-sum;}
-			ans=Math.max(ans,diff);
-			if(sum==target){
-				return 0;
-			}
-			else if(sum<target){j++;}
-			else{k--;}
-		}
-	}
-  return ans;
+  arr.sort((a,b) => a-b);
+  let minDiff = Infinity;
+  let res = 0;
+  for (let i = 0; i < arr.length-2; i++) {
+    let j = i+1, k = arr.length-1;
+  
+    while (j < k) {
+      let threeSum = arr[i] + arr[j] + arr[k];
+
+      if(Math.abs(threeSum-target) < minDiff) {
+        minDiff = Math.abs(threeSum-target)
+        res = threeSum;
+      }
+
+      if(threeSum > target) k--;
+      else if (threeSum < target) j++;
+      else return target;
+    }
+    while (i < arr.length-2 && arr[i] === arr[i+1]) {
+      i++;
+    }
+  }
+    
+  return res;
 }
 
 module.exports = threeSum;
